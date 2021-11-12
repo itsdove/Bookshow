a603c57f9f2f49b268d47dc1827d7ae4389dc95a
itsdove <lianximing@icloud.com>
2021-11-12 21:32:12 +0800

    大改.1

--- app/src/main/java/com/example/bookshow/Fragmentmj.java
+++ app/src/main/java/com/example/bookshow/Fragmentmj.java
@@ -9,12 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class Fragmentmj extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragmentmj, container);
+        WebView webView=view.findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.baidu.com/");
         return view;
     }
 }
--- app/src/main/java/com/example/bookshow/Fragmentxw.java
+++ app/src/main/java/com/example/bookshow/Fragmentxw.java
@@ -9,11 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class Fragmentxw extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragmentxw, container);
+        WebView webView=view.findViewById(R.id.webview2);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.bilibili.com/");
         return view;
     }
 }
